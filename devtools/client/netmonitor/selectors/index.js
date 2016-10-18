74c01540a7e4084c6ac628c7fa002a58fa3e931d
Jarda Snajdr <jsnajdr@gmail.com>
2016-10-18 15:36:42 +0200

    Selectors use reselect
    MozReview-Commit-ID: ByJCOH3G7Y3

--- devtools/client/netmonitor/selectors/index.js
+++ devtools/client/netmonitor/selectors/index.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
+const { createSelector } = require("devtools/client/shared/vendor/reselect");
 const { Filters, isFreetextMatch } = require("../filter-predicates");
 const { Sorters } = require("../sort-predicates");
 
@@ -10,81 +11,86 @@ const { Sorters } = require("../sort-predicates");
  * Check if the given requests is a clone, find and return the original request if it is.
  * Cloned requests are sorted by comparing the original ones.
  */
-function getOrigRequest(state, req) {
+function getOrigRequest(requests, req) {
   if (!req.id.endsWith("-clone")) {
     return req;
   }
 
   const origId = req.id.replace(/-clone$/, "");
-  return getRequestById(state, origId);
+  return requests.find(r => r.id === origId);
 }
 
-function getFilter(state) {
-  const { enabled, text } = state.filter;
-  return r => enabled.some(p => Filters[p] && Filters[p](r.data)) &&
-              isFreetextMatch(r.data, text);
-}
+const getFilterFn = createSelector(
+  state => state.filter,
+  filter => {
+    return r => filter.enabled.some(p => Filters[p] && Filters[p](r.data)) &&
+                isFreetextMatch(r.data, filter.text);
+  }
+);
 
-function getSorter(state) {
-  const { sortBy } = state;
+const getSortFn = createSelector(
+  state => state.requests,
+  state => state.sortBy,
+  (requests, sortBy) => {
+    let dataSorter = Sorters[sortBy.type || "waterfall"];
 
-  let dataSorter = Sorters[sortBy.type || "waterfall"];
+    function sortWithClones(a, b) {
+      // If one request is a clone of the other, sort them next to each other
+      if (a.id == b.id + "-clone") {
+        return +1;
+      } else if (a.id + "-clone" == b.id) {
+        return -1;
+      }
 
-  function sortWithClones(a, b) {
-    // If one request is a clone of the other, sort them next to each other
-    if (a.id == b.id + "-clone") {
-      return +1;
-    } else if (a.id + "-clone" == b.id) {
-      return -1;
+      // Otherwise, get the original requests and compare them
+      return dataSorter(
+        getOrigRequest(requests, a).data,
+        getOrigRequest(requests, b).data
+      );
     }
 
-    // Otherwise, get the original requests and compare them
-    return dataSorter(
-      getOrigRequest(state, a).data,
-      getOrigRequest(state, b).data
-    );
+    const ascending = sortBy.ascending ? +1 : -1;
+    return (a, b) => ascending * sortWithClones(a, b, dataSorter);
   }
+);
 
-  const ascending = sortBy.ascending ? +1 : -1;
-  return (a, b) => ascending * sortWithClones(a, b, dataSorter);
-}
+const getSortedRequests = createSelector(
+  state => state.requests,
+  getSortFn,
+  (requests, sortFn) => requests.sort(sortFn)
+);
 
-function getSortedRequests(state) {
-  const { requests } = state;
-  const sortFn = getSorter(state);
-  return requests.sort(sortFn);
-}
+const getDisplayedRequests = createSelector(
+  state => state.requests,
+  getFilterFn,
+  getSortFn,
+  (requests, filterFn, sortFn) => requests.filter(filterFn).sort(sortFn)
+);
 
-function getDisplayedRequests(state) {
-  const { requests } = state;
-  const filterFn = getFilter(state);
-  const sortFn = getSorter(state);
-  return requests.filter(filterFn).sort(sortFn);
-}
+const getDisplayedRequestsSummary = createSelector(
+  getDisplayedRequests,
+  (requests) => {
+    if (requests.size == 0) {
+      return { count: 0, bytes: 0, millis: 0 };
+    }
 
-function getDisplayedRequestsSummary(state) {
-  const requests = getDisplayedRequests(state);
+    const totalBytes = requests.reduce((total, item) => {
+      let size = item.data.contentSize;
+      return total + (typeof size == "number" ? size : 0);
+    }, 0);
 
-  if (requests.size == 0) {
-    return { count: 0, bytes: 0, millis: 0 };
+    const oldestRequest = requests.reduce(
+      (prev, curr) => prev.data.startedMillis < curr.data.startedMillis ? prev : curr);
+    const newestRequest = requests.reduce(
+      (prev, curr) => prev.data.startedMillis > curr.data.startedMillis ? prev : curr);
+
+    return {
+      count: requests.size,
+      bytes: totalBytes,
+      millis: newestRequest.data.endedMillis - oldestRequest.data.startedMillis,
+    };
   }
-
-  const totalBytes = requests.reduce((total, item) => {
-    let size = item.data.contentSize;
-    return total + (typeof size == "number" ? size : 0);
-  }, 0);
-
-  const oldestRequest = requests.reduce(
-    (prev, curr) => prev.data.startedMillis < curr.data.startedMillis ? prev : curr);
-  const newestRequest = requests.reduce(
-    (prev, curr) => prev.data.startedMillis > curr.data.startedMillis ? prev : curr);
-
-  return {
-    count: requests.size,
-    bytes: totalBytes,
-    millis: newestRequest.data.endedMillis - oldestRequest.data.startedMillis,
-  };
-}
+);
 
 function getRequestById(state, id) {
   return state.requests.find(r => r.id === id);
